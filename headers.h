289175621cce23a6b3a62ebf0d6f4de21dda7b8c
Techtastic <153880417+Techtastic88@users.noreply.github.com>
2025-10-18 12:41:07 -0700

    Create headers.h

---
+++ headers.h
@@ -0,0 +1,17 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <syscall.h>
+#include <limits.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <math.h>
+#include <stdarg.h>
+#endif 
